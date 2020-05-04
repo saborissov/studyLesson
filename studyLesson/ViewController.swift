064d4e028c7374433ff4b4231a39bbe96d46bdc3
SergeyB <saborissov@mail.ru>
2020-05-04 21:05:17 +0300

    Initial Commit

---
+++ studyLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  studyLesson
+//
+//  Created by Сергей Борисов on 04.05.2020.
+//  Copyright © 2020 Сергей Борисов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
